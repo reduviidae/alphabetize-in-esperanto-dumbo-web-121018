956b7d23ef5ffc2191b21b36f4acfc8e9361d5fa
Wil Cook <wil.cook.is@gmail.com>
2018-12-04 20:31:35 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  # binding.pry
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by{|string| string.tr(esperanto, ascii)}
 end
